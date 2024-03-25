fe2c823959d05d7d6c85103f17766894e4e62616
Kasper Sprengers <diabl0570@hotmail.com>
2024-03-25 20:44:20 +0100

    first commit

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      devShells = forEachSystem
+        (system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+          {
+            default = devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [
+                {
+                  # https://devenv.sh/reference/options/
+                  packages = with pkgs; [
+                    git
+                    nodePackages_latest.vercel
+                    nodePackages.pnpm
+                  ];
+
+                  enterShell = ''
+                  '';
+
+                  languages.typescript.enable = true;
+                  languages.javascript.enable = true;
+                  languages.javascript.package = pkgs.nodejs_20;
+                }
+              ];
+            };
+          });
+    };
+}
